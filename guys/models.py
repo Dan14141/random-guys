df180a308e73aec9c51caecaeeb5e51b7e1d1a33
Dan14141 <dan.sim2014@yandex.ru>
2026-05-19 07:55:43 +0300

    Added model and connect database

---
+++ guys/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Guy(models.Model):
+    """модель для хранения информации о человеке"""
+    GENDER_CHOICES = (('man', 'Мужчина'),('woman', 'Женщина'))
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, verbose_name="Пол")
+    first_name = models.CharField(max_length=30, verbose_name="Имя")
+    last_name = models.CharField(max_length=30, verbose_name="Фамилия")
+    phone = models.CharField(max_length=20, verbose_name="Номер телефона")
+    email = models.EmailField(verbose_name="Электронная почта")
+    address = models.CharField(max_length=255, verbose_name="Адрес")
+
+    def __str__(self):
+        """строковое представление объекта: фамилия имя"""
+        return f"{self.last_name} {self.first_name}"
